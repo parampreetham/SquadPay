4f257b2dc047708a91f01c17ef333c04956147f2
parampreetham <parampreetham@gmail.com>
2025-11-23 12:35:20 +0530

    Fix router basename and receipt links for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SquadPay">
       <App />
     </BrowserRouter>
   </React.StrictMode>
