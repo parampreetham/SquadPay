8d937fa0ac020f4c21050f9b548b08591f5fe078
parampreetham <parampreetham@gmail.com>
2025-11-23 13:22:37 +0530

    Add SPA 404 fallback in GitHub Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // correct for custom domain
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "index.html"), // 👈 this is the fix
-      }
-    }
-  }
+  base: "/", // custom domain -> root
 });
